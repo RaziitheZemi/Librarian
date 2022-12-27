77a73f54e8cae455159a7fd44801080eca1b12ca
RaziitheZemi <65795468+RaziitheZemi@users.noreply.github.com>
2022-12-27 15:08:30 -0500

    Update main.js

--- client/main.js
+++ client/main.js
@@ -74,7 +74,7 @@ const handleSubmit = async (e) => {
 
     //fetch data from server -> bot's responce
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://talkbot3000.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
